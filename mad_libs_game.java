4c447e571fb338f14a6bf3bfe2d1cd670f9dee7c
SARAVANA KUMAR G <kumargsaravanak@gmail.com>
2026-04-19 23:36:56 +0530

    Add files via upload

---
+++ mad_libs_game.java
@@ -0,0 +1,43 @@
+package basics; // My Package
+import java.util.Scanner;
+
+public class mad_libs_game {
+    public static void main(String[] args) {
+        System.out.println("");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String noun1;
+        String adjective1;
+        String verb1;
+        String verb2;
+        String noun2;
+        String adjective2;
+
+        System.out.print("Enter a noun: ");
+        noun1 = scanner.nextLine();
+
+        System.out.print("Enter a adjective: ");
+        adjective1 = scanner.nextLine();
+
+        System.out.print("Enter a verb: ");
+        verb1 = scanner.nextLine();
+
+        System.out.print("Enter another verb: ");
+        verb2 = scanner.nextLine();
+
+        System.out.print("Enter a noun: ");
+        noun2 = scanner.nextLine();
+        
+        System.out.print("Enter a adjective: ");
+        adjective2 = scanner.nextLine();
+
+        System.out.println("");
+        System.out.println("Hi, I am " + noun1 + " with " + adjective1 + " hair.");
+        System.out.println(noun1 + " " + verb1 + " some food to " + verb2 + ".");
+        System.out.println("It can be a " + noun2 + " which should be " + adjective2 + ".");
+
+        System.out.println("");
+        scanner.close();
+    }
+}
---
+++ shopping_cart_program.java
@@ -0,0 +1,35 @@
+package basics;
+
+import java.util.Scanner;
+
+public class shopping_cart_program {
+    public static void main(String[] args) {
+        System.out.println("");
+
+        // Shopping Cart Program
+
+        String itemName;
+        double price;
+        int quantity;
+        double total;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the name of an item: ");
+        itemName = scanner.nextLine();
+
+        System.out.print("Enter the Price of the item: ");
+        price = scanner.nextDouble();
+
+        System.out.print("How many do you want? : ");
+        quantity = scanner.nextInt();
+
+        total = price * quantity;
+
+        System.out.println("");
+        System.out.println("You ordered " + quantity + " " + itemName + " and the total is Rs." + total);
+
+        System.out.println("");
+        scanner.close();
+    }
+}
